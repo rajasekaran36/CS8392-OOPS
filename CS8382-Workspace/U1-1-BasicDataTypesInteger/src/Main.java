64278d0b2cf60b4751f4898adb4c810b0229d6b5
Rajasekaran S <proffraja@gmail.com>
2018-07-08 23:00:43 +0530

    Eclipse Workspace and PPT Added

---
+++ CS8382-Workspace/U1-1-BasicDataTypesInteger/src/Main.java
@@ -0,0 +1,32 @@
+/*==================================================
+ * @author Rajasekaran S
+ * @version 1.0
+ * Subject:Object Oriented Programming
+ * Subject Code:CS8382
+ * Concept:Demo of Basic Integer Data Types of Java 
+ ===================================================*/
+public class Main {
+
+	public static void main(String[] args) {
+		int i;
+		short s;
+		long l;
+		byte b;
+		
+		/*------Random Numbers------*/
+		i = 88383;
+		s = 3793;
+		l = 973792977;
+		b = 120;
+		
+		System.out.println("Basic Integer Types");
+		System.out.println("-------------------");
+		System.out.println("[Integer] Value of i :"+i);
+		System.out.println("[Short]   Value of s :"+s);
+		System.out.println("[Long] Value of l"+l);
+		System.out.println("[Byte] Value of b:"+b);
+		
+		/*------Random Numbers------*/
+	}
+
+}
