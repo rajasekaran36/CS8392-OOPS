12ecde87b2dcffd64628e9bda781ce7c5d5c7416
Rajasekaran S <proffraja@gmail.com>
2018-07-16 09:33:08 +0530

    Message

---
+++ Class Exam/Simple Class/Main.java
@@ -0,0 +1,64 @@
+import java.io.*;
+class Student{
+int rollno;
+String name;
+
+//Constructor with out arguments-Default Constructor
+
+Student(){
+System.out.println("Default Constructor Called");
+rollno = -1;
+name = "No name";
+}
+
+//Constructor with arguments-Parameterised Constructor
+
+Student(int rno,String n){
+System.out.println("Parameterised Constructor Called");
+rollno = rno;
+name = n;
+}
+
+//Get the details from user and set
+
+void setDetails() throws IOException {
+String s_rollno;
+InputStreamReader insr = new InputStreamReader(System.in);
+BufferedReader bfr = new BufferedReader(insr);
+
+System.out.println("Enter Student Rollno");
+s_rollno = bfr.readLine();
+rollno = Integer.parseInt(s_rollno);
+
+System.out.println("Enter Student Name:");
+name = bfr.readLine();
+ 
+}
+
+//Display Student Details
+
+void getDetails(){
+System.out.println("Student Details");
+System.out.println("---------------");
+System.out.println("Rollno:"+rollno);
+System.out.println("Name:"+name);
+}
+
+}
+
+class Main{
+
+public static void main(String[] args)throws IOException{
+
+Student s=new Student();
+s.getDetails();
+
+Student m=new Student(13,"Bala");
+m.getDetails(); 
+
+Student l = new Student();
+l.setDetails();
+l.getDetails();
+
+}
+}
