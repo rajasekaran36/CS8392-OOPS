fb2f78cc3435e9976c8e2375c476f30cfeaecf53
Rajasekaran <proffraja@gmail.com>
2019-07-11 16:46:38 +0530

    Last Update 11-07-2019 16:46:37.90

--- ex/jadoc/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main{
-	public static void main(String[] args){
-	Cycle a = new Cycle("Hero", 5645,"Red");
-	a.displayCycle();
-	System.out.println("Brand of the Cycle is:"+a.getBrand());
-	}
-}
